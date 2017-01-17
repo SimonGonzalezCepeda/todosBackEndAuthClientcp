ad7d9a1b131b42bc79deae73fda2cad121a25dc8
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2017-01-17 19:25:51 +0100

    Copied everything

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    //
+    /**
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $access_token = null;
+        try {
+            $access_token = AccesToken:where('user_id',1)->token;
+        } catch (\Exception) {
+            //Execute code to obtain token
+            // Redirect to /redirect
+        }
+
+        $data = [
+            "access_token" => $access_token
+        ];
+        return view('tasks',$data);
+    }
+
+}
